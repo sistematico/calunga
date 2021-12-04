1e2642faef4319cda98252f9feb7b3f4ce46f33c
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-12-03 21:51:32 -0400

    Update

--- calunga.py
+++ calunga.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-import os
+import os, time
+#from datetime import datetime, timedelta
 import youtube_dl
 from telegram import Update
 from telegram.ext import Updater, MessageHandler, CallbackContext, Filters
 from dotenv import load_dotenv
+
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 DOWNLOAD = 'downloads/'
 
+def older(dir_path, n):
+    all_files = os.listdir(dir_path)
+    now = time.time()
+    #max = n * 86400
+    max = 60
+    for f in all_files:
+        file_path = os.path.join(dir_path, f)
+        if not os.path.isfile(file_path):
+            continue
+        if os.stat(file_path).st_mtime < now - max:
+            os.remove(file_path)
+            print("Deleted ", f)
+
 def download(update: Update, context: CallbackContext):
+    older(DOWNLOAD, 7)
+
     url = update.message.text
+
     opts = {
         'format': 'best',
         'ignoreerrors': True,
@@ -21,19 +39,18 @@ def download(update: Update, context: CallbackContext):
         'youtube_include_dash_manifest': False,
         'socket_timeout': 8,
         'retries': 3,
-        'outtmpl': DOWNLOAD + '/%(title)s-%(id)s.%(ext)s',
+        'outtmpl': DOWNLOAD + '%(title)s-%(id)s.%(ext)s',
     }
+
     with youtube_dl.YoutubeDL(opts) as ydl:
-        update.message.reply_text('Baixando: ' + url,quote=True, disable_web_page_preview=True)
+        update.message.reply_text('Baixando: ' + url, quote=True, disable_web_page_preview=True)
         try:
-            # ydl.download([url])
             result = ydl.extract_info(url, download=True)
 
             if 'entries' in result:
                 video = result['entries'][0]
             else:
                 video = result
-
             
             try:
                 filename = open(ydl.prepare_filename(video), 'rb')
@@ -43,21 +60,10 @@ def download(update: Update, context: CallbackContext):
             finally:
                 filename.close()
 
-
-            #update.message.reply_text(ydl.prepare_filename(video),quote=True)
-            #update.message.reply_video(ydl.prepare_filename(video),quote=True)
-            #update.message.reply_video(ydl.prepare_filename(video),quote=True)
-
-            #bot.send_video(chat_id=update.message.chat_id, video=open('output.mp4', 'rb'), supports_streaming=True)
-            #update.message.reply_video(video=open(ydl.prepare_filename(video), 'rb'), supports_streaming=True)
-
         except:
-            update.message.reply_text('Um erro ocorreu',quote=True)    
-
+            update.message.reply_text('Um erro ocorreu, tente novamente.',quote=True)    
 
 updater = Updater(TOKEN, use_context=True)
-#updater.dispatcher.add_handler(MessageHandler(Filters.text, download))
-#updater.dispatcher.add_handler(MessageHandler(Filters.text, download))
 updater.dispatcher.add_handler(MessageHandler(Filters.entity('url'), download))
 
 updater.start_polling()
--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-if [ "$EUID" -ne 0 ]; then
-    echo "Este script precisa rodar como root"
-    exit
-fi
+# [ "$EUID" -ne 0 ] && echo "Este script precisa rodar como root" && exit
 
-systemctl daemon-reload
+[ "$EUID" -ne 0 ] && systemctl daemon-reload
 
 if ! id "calunga" &>/dev/null; then
     useradd -m -r -d /var/calunga -s /bin/bash calunga
@@ -15,10 +12,10 @@ fi
 [ ! -f /etc/systemd/system/calunga.service ] && cp etc/systemd/system/calunga.service /etc/systemd/system/
 [ ! -f /etc/sudoers.d/95-calunga ] && cp etc/sudoers.d/95-calunga /etc/sudoers.d/
 
-chmod 440 /etc/sudoers.d/95-calunga
+[ "$EUID" -ne 0 ] && chmod 440 /etc/sudoers.d/95-calunga
 
 if [ "$(systemctl is-active calunga)" == "inactive" ]; then
-    systemctl enable calunga
+    [ "$EUID" -ne 0 ] && systemctl enable calunga || sudo systemctl enable calunga
 fi
 
-systemctl restart calunga
+[ "$EUID" -ne 0 ] && systemctl restart calunga || sudo systemctl restart calunga
