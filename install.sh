dca74055028ec9e3bcd6c7dbcbecf04591962788
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-12-03 23:39:26 -0400

    Update

--- install.sh
+++ install.sh
@@ -18,4 +18,4 @@ if [ "$(systemctl is-active calunga)" == "inactive" ]; then
     [ "$EUID" -eq 0 ] && systemctl enable calunga || sudo systemctl enable calunga
 fi
 
-#[ "$EUID" -eq 0 ] && systemctl restart calunga || sudo systemctl restart calunga
+[ "$EUID" -eq 0 ] && systemctl restart calunga || sudo systemctl restart calunga
